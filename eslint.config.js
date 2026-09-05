9699877e4cd67d70b36b8cb17540b7688c6609d9
恒宇少年 <jnyuqy@gmail.com>
2026-09-05 16:53:52 +0800

    fix(tooling): 忽略临时工作树

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist/', 'src/generated/', 'eslint.config.js'] },
+  { ignores: ['dist/', 'src/generated/', '.worktrees/', 'eslint.config.js'] },
   eslint.configs.recommended,
   ...tseslint.configs.strictTypeChecked,
   {
